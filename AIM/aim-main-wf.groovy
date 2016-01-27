2fb1d9cd139cc82f22cdad001a31e0bb3f92d4fa
mvpartisans <imran.iansari@yahoo.com>
2016-01-27 17:30:18 -0500

    remove cigna

--- AIM/aim-main-wf.groovy
+++ AIM/aim-main-wf.groovy
@@ -4,26 +4,3 @@
     
     acme = new Acme();
     acme.say('imran');
-
-standardCignaBuild {
-
-    envVars = {
-        mavenVersion = 'Maven 3.3';
-        jdkVersion = 'JDK8'
-    }
-
-    preBuild = {
-        println 'pre build from dev'
-    }
-
-    build = {
-        println 'Build some project'
-        println 'git repo'
-        println 'compile & build'
-    }
-
-    postBuild = {
-        println 'post build from dev'
-    }
-
-}
