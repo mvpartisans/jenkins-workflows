c94614ec783030867de1e2f3447a03bb361b0c26
mvpartisans <imran.iansari@yahoo.com>
2016-01-27 17:30:59 -0500

    redo

--- AIM/aim-main-wf.groovy
+++ AIM/aim-main-wf.groovy
@@ -4,3 +4,27 @@
     
     acme = new Acme();
     acme.say('imran');
+
+
+standardCignaBuild {
+
+    envVars = {
+        mavenVersion = 'Maven 3.3';
+        jdkVersion = 'JDK8'
+    }
+
+    preBuild = {
+        println 'pre build from dev'
+    }
+
+    build = {
+        println 'Build some project'
+        println 'git repo'
+        println 'compile & build'
+    }
+
+    postBuild = {
+        println 'post build from dev'
+    }
+
+}
